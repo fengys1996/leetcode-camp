076b8546db74ca5faf69c9a6c942d6bd22220ef4
fys <1113014250@qq.com>
2021-08-18 23:04:08 +0800

    leetcode no 35 rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ mod no20;
 mod no21;
 mod no26;
 mod no27;
+mod no35;
 mod no74;
 
 fn main() {
---
+++ rust/src/no35.rs
@@ -0,0 +1,54 @@
+use std::cmp::Ordering;
+
+struct Solution;
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let lenght = nums.len();
+        if lenght == 0 {
+            return 0;
+        }
+        let mut left: i32 = 0;
+        let mut right: i32 = (lenght - 1) as i32;
+        let mut mid: i32 = 0;
+        while left <= right {
+            mid = (left + right) / 2;
+            match nums[mid as usize].cmp(&target) {
+                Ordering::Equal => return mid,
+                Ordering::Greater => right = mid - 1,
+                Ordering::Less => left = mid + 1,
+            }
+        }
+        if nums[mid as usize] > target {
+            return mid;
+        }
+        mid + 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::no35::Solution;
+
+    #[test]
+    fn test_search_insert() {
+        let mut nums = Vec::new();
+        nums.push(1);
+        nums.push(2);
+        nums.push(3);
+        nums.push(5);
+        assert_eq!(1, Solution::search_insert(nums.clone(), 2));
+        assert_eq!(3, Solution::search_insert(nums.clone(), 4));
+
+        let mut nums = Vec::new();
+        nums.push(1);
+        assert_eq!(0, Solution::search_insert(nums.clone(), 0));
+        assert_eq!(0, Solution::search_insert(nums.clone(), 1));
+        assert_eq!(1, Solution::search_insert(nums.clone(), 2));
+
+        let nums = Vec::new();
+        assert_eq!(0, Solution::search_insert(nums.clone(), 0));
+        assert_eq!(0, Solution::search_insert(nums.clone(), 1));
+    }
+}

224acdb8e3f54b04a34f1713b477de73a7d06cd7
fys <1113014250@qq.com>
2021-08-19 23:42:46 +0800

    leetcode no 349 rust optimization

--- rust/src/no349.rs
+++ rust/src/no349.rs
@@ -5,9 +5,13 @@ struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let set1: HashSet<i32> = nums1.into_iter().collect();
         let set2: HashSet<i32> = nums2.into_iter().collect();
-        set1.into_iter().filter(|x| set2.contains(x)).collect()
+        nums1
+            .into_iter()
+            .filter(|item| set2.contains(item))
+            .collect::<HashSet<i32>>()
+            .into_iter()
+            .collect()
     }
 }
 

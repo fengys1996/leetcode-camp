74cf92ecc6428389b2a8aa581c5d3cd1049521c5
fys <1113014250@qq.com>
2021-08-22 11:40:00 +0800

    leetcode no 33 rust greater

--- rust/src/no33.rs
+++ rust/src/no33.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 struct Solution;
 
 impl Solution {
@@ -6,6 +8,33 @@ impl Solution {
         for (i, &item) in nums.iter().enumerate() {
             if item == target {
                 return i as i32;
+            }
+        }
+        -1
+    }
+
+    #[allow(dead_code)]
+    pub fn search_great(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left: i32 = 0;
+        let mut right: i32 = nums.len() as i32 - 1;
+        while left <= right {
+            let mid = left + ((right - left) >> 1);
+            match nums[mid as usize].cmp(&target) {
+                Ordering::Equal => return mid as i32,
+                Ordering::Greater => {
+                    if nums[left as usize] <= target {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                Ordering::Less => {
+                    if nums[right as usize] >= target {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
             }
         }
         -1
@@ -25,5 +54,24 @@ mod test {
         let nums = vec![0];
         let target = 0;
         assert_eq!(0, Solution::search(nums, target));
+
+        let nums = vec![1];
+        let target = 2;
+        assert_eq!(-1, Solution::search(nums, target));
+    }
+
+    #[test]
+    fn test_search_great() {
+        let nums = vec![4, 5, 6, 7, 0, 1, 2];
+        let target = 0;
+        assert_eq!(4, Solution::search_great(nums, target));
+
+        let nums = vec![0];
+        let target = 0;
+        assert_eq!(0, Solution::search_great(nums, target));
+
+        let nums = vec![1];
+        let target = 2;
+        assert_eq!(-1, Solution::search_great(nums, target));
     }
 }

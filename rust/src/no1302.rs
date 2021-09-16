8c7ca88911779c01ef12509c1eb808c5a557144d
fys <1113014250@qq.com>
2021-09-17 00:28:29 +0800

    leetcode no 1302 rust greater

--- rust/src/no1302.rs
+++ rust/src/no1302.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, cmp::Ordering, collections::VecDeque, rc::Rc};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -38,9 +38,9 @@ impl Solution {
             let mut r = node.borrow_mut();
             if r.left.is_none() && r.right.is_none() {
                 match lvl.cmp(&MAX_LVL) {
-                    std::cmp::Ordering::Less => return,
-                    std::cmp::Ordering::Equal => SUM += r.val,
-                    std::cmp::Ordering::Greater => {
+                    Ordering::Less => return,
+                    Ordering::Equal => SUM += r.val,
+                    Ordering::Greater => {
                         SUM = r.val;
                         MAX_LVL = lvl;
                     }
@@ -49,6 +49,34 @@ impl Solution {
             Self::dfs(r.left.take(), lvl + 1);
             Self::dfs(r.right.take(), lvl + 1);
         }
+    }
+
+    #[allow(dead_code)]
+    pub fn deepest_leaves_sum_greater(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut max_lvl = 0;
+        let mut sum = 0;
+        let mut queue = VecDeque::new();
+        queue.push_back((root.unwrap(), 0));
+        while !queue.is_empty() {
+            let node = queue.pop_front().unwrap().clone();
+            let depth = node.1;
+            let node = node.0.borrow_mut();
+            if let Some(new_node) = node.left.clone() {
+                queue.push_back((new_node, depth + 1));
+            }
+            if let Some(new_node) = node.right.clone() {
+                queue.push_back((new_node, depth + 1));
+            }
+            match depth.cmp(&max_lvl) {
+                Ordering::Less => {}
+                Ordering::Equal => sum += node.val,
+                Ordering::Greater => {
+                    max_lvl = depth;
+                    sum = node.val;
+                }
+            }
+        }
+        sum
     }
 }
 
@@ -87,5 +115,32 @@ mod tests {
                 Solution::deepest_leaves_sum(Some(Rc::new(RefCell::new(root))))
             );
         }
+    }
+
+    #[test]
+    fn test_deepest_leaves_sum_grater_1() {
+        assert_eq!(0, Solution::deepest_leaves_sum_greater(None));
+        let root = TreeNode::new(1);
+        assert_eq!(
+            1,
+            Solution::deepest_leaves_sum_greater(Some(Rc::new(RefCell::new(root))))
+        );
+    }
+
+    #[test]
+    fn test_deepest_leaves_sum_greater_2() {
+        let mut root = TreeNode::new(1);
+        let mut children1_1 = TreeNode::new(2);
+        let mut children1_2 = TreeNode::new(3);
+        let children2_1 = TreeNode::new(4);
+        let children2_2 = TreeNode::new(5);
+        children1_1.left = Some(Rc::new(RefCell::new(children2_1)));
+        children1_2.right = Some(Rc::new(RefCell::new(children2_2)));
+        root.left = Some(Rc::new(RefCell::new(children1_1)));
+        root.right = Some(Rc::new(RefCell::new(children1_2)));
+        assert_eq!(
+            9,
+            Solution::deepest_leaves_sum_greater(Some(Rc::new(RefCell::new(root))))
+        );
     }
 }

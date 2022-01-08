4fd70b249c27ab936eba4aa23e7f0400103dab99
fys <1113014250@qq.com>
2022-01-08 12:58:59 +0800

    leetcode no 1438 rust

--- rust/src/no1438.rs
+++ rust/src/no1438.rs
@@ -1,4 +1,7 @@
-use std::{cmp::max, collections::BTreeMap};
+use std::{
+    cmp::max,
+    collections::{BTreeMap, VecDeque},
+};
 struct Solution;
 
 impl Solution {
@@ -27,6 +30,38 @@ impl Solution {
         }
         res as i32
     }
+
+    #[allow(dead_code)]
+    pub fn longest_subarray_1(nums: Vec<i32>, limit: i32) -> i32 {
+        let mut left = 0;
+        let mut right = 0;
+        let mut res = 0;
+        let mut min_queue: VecDeque<i32> = VecDeque::new();
+        let mut max_queue: VecDeque<i32> = VecDeque::new();
+        let len = nums.len();
+        while right < len {
+            while !min_queue.is_empty() && nums[right] < *min_queue.back().unwrap() {
+                min_queue.pop_back();
+            }
+            while !max_queue.is_empty() && nums[right] > *max_queue.back().unwrap() {
+                max_queue.pop_back();
+            }
+            min_queue.push_back(nums[right]);
+            max_queue.push_back(nums[right]);
+            while max_queue.front().unwrap() - min_queue.front().unwrap() > limit {
+                if nums[left] == *max_queue.front().unwrap() {
+                    max_queue.pop_front();
+                }
+                if nums[left] == *min_queue.front().unwrap() {
+                    min_queue.pop_front();
+                }
+                left += 1;
+            }
+            res = max(res, right - left + 1);
+            right += 1;
+        }
+        res as i32
+    }
 }
 
 #[cfg(test)]
@@ -38,5 +73,17 @@ mod tests {
         assert_eq!(2, Solution::longest_subarray(vec![8, 2, 4, 7], 4));
         assert_eq!(4, Solution::longest_subarray(vec![10, 1, 2, 4, 7, 2], 5));
         assert_eq!(3, Solution::longest_subarray(vec![2, 2, 2, 4, 4, 2, 2], 0));
+        assert_eq!(3, Solution::longest_subarray(vec![8, 7, 6, 5], 2));
+    }
+
+    #[test]
+    fn test_longest_subarray_1() {
+        assert_eq!(2, Solution::longest_subarray_1(vec![8, 2, 4, 7], 4));
+        assert_eq!(4, Solution::longest_subarray_1(vec![10, 1, 2, 4, 7, 2], 5));
+        assert_eq!(
+            3,
+            Solution::longest_subarray_1(vec![2, 2, 2, 4, 4, 2, 2], 0)
+        );
+        assert_eq!(3, Solution::longest_subarray_1(vec![8, 7, 6, 5], 2));
     }
 }
